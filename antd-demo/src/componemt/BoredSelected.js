553dd2f8c92d92ac7530436a30ca57509a3c41ab
DESKTOP-7M34HLI\siwen <siwenjuan@maxtropy.com>
2017-10-20 11:19:25 +0800

    add ant-design

---
+++ antd-demo/src/componemt/BoredSelected.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Button } from 'antd'
+
+class BoredSelected extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <p className='App-intro'>
+          <Button type='primary'>Button</Button>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default BoredSelected
