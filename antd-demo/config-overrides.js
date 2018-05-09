88b8771e0902b55889e1b5471d0be28feb56f18f
DESKTOP-7M34HLI\siwen <siwenjuan@maxtropy.com>
2018-05-09 17:45:26 +0800

    some change

--- antd-demo/config-overrides.js
+++ antd-demo/config-overrides.js
@@ -1,4 +1,4 @@
-const { injectBabelPlugin } = require('react-app-rewired')
+const { injectBabelPlugin } = require('react-app-rewired') // 按需加载样组件和样式的插件
 module.exports = function override (config, env) {
   config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config)
   // do stuff with the webpack config...
--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
-import BoredSelected from './componemt/BoredSelected'
+import BoredSelected from './container/BoredSelected'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <BoredSelected />
+        antd项目
       </div>
     )
   }
