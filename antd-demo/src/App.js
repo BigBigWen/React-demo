a1c4315a7d1e4498fe5b751a0dd244c74586ffc6
DESKTOP-7M34HLI\siwen <siwenjuan@maxtropy.com>
2019-02-12 11:45:35 +0800

    add react-dnd-demo

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react'
+import {Router, Route, Link, Switch} from 'react-router'
 import './App.css'
-// import BoredSelected from './container/BoredSelected'
+import BoredSelected from './container/BoredSelected'
 import TreeG2 from './container/TreeG2'
 // import NodeTree from './container/NodeTree'
 import TreeG6 from './container/TreeG6'
 import TextTree from './container/TextTree'
 import CustomNode from './container/CustomNode'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        {/* <BoredSelected /> */}
-        antd项目
-        <TreeG2 />
-        <div>---------------------------------------华丽分割线-------------------------------------</div>
-        {/* <NodeTree /> */}
-        <TreeG6 />
-        <div>---------------------------------------华丽分割线-------------------------------------</div>
-        <TextTree />
-        <div>---------------------------------------华丽分割线-------------------------------------</div>
-        <CustomNode />
-      </div>
-    )
-  }
+// class App extends Component {
+//   render () {
+//     return (
+//       <div className='App'>
+//         {/* <BoredSelected /> */}
+//         antd项目
+//         <TreeG2 />
+//         <div>---------------------------------------华丽分割线-------------------------------------</div>
+//         {/* <NodeTree /> */}
+//         <TreeG6 />
+//         <div>---------------------------------------华丽分割线-------------------------------------</div>
+//         <TextTree />
+//         <div>---------------------------------------华丽分割线-------------------------------------</div>
+//         <CustomNode />
+//       </div>
+//     )
+//   }
+// }
+
+const App = ({ history }) => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/' component={CustomNode} />
+        <Route exact path='/bored-selected' component={BoredSelected} />
+      </Switch>
+    </Router>
+
+  )
 }
 
 export default App
--- antd-demo/src/index.js
+++ antd-demo/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 // import './index.css'
 import './style.css'
 import App from './App'
+import history from './lib/histropy'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App history={history} />, document.getElementById('root'))
 registerServiceWorker()
